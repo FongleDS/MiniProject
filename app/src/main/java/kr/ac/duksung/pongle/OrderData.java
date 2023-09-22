a2e9c571f51a8f0cf60ee921ed9d8c84a2166419
violet <101194343+joococo@users.noreply.github.com>
2023-09-22 16:59:44 +0900

    init

---
+++ app/src/main/java/kr/ac/duksung/pongle/OrderData.java
@@ -0,0 +1,40 @@
+package kr.ac.duksung.pongle;
+
+public class OrderData {
+    private static final OrderData instance = new OrderData();
+
+    private String seatID;
+    private String menuID;
+    private String stdID;
+
+    private OrderData() {}
+
+    public static OrderData getInstance() {
+        return instance;
+    }
+
+    public String getSeatID() {
+        return seatID;
+    }
+
+    // Setter for name
+    public void setSeatID(String seatID) {
+        this.seatID = seatID;
+    }
+
+    public String getMenuID() {
+        return menuID;
+    }
+
+    public void setMenuID(String menuID) {
+        this.menuID = menuID;
+    }
+
+    public String getStdID() {
+        return stdID;
+    }
+
+    public void setStdID(String stdID) {
+        this.stdID = stdID;
+    }
+}
