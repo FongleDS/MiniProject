8e9012d1ec50dec7c3a05ab64e42a7751051b7af
DESKTOP-LN9PSIC\82102 <ejwuud2@naver.com>
2023-09-26 18:15:42 +0900

    메뉴고르면 checkinfo

--- app/src/main/java/kr/ac/duksung/pongle/ChooseMenu.java
+++ app/src/main/java/kr/ac/duksung/pongle/ChooseMenu.java
@@ -1,7 +1,10 @@
 package kr.ac.duksung.pongle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -10,12 +13,13 @@ import java.util.Locale;
 public class ChooseMenu extends AppCompatActivity {
 
     OrderData orderData = new OrderData();
-
+    Button btn_choose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_menu);
+        btn_choose = findViewById(R.id.btn_choose);
 
         // 실시간 현재 시간 받아오기
         Calendar calendar = Calendar.getInstance();
@@ -24,5 +28,13 @@ public class ChooseMenu extends AppCompatActivity {
 
         //메뉴 정보 넣기
         //orderData.setMenuID();
+
+        btn_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CheckInfo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
