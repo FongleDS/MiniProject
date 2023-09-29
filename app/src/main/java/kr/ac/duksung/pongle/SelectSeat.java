6c299c39bec5ea66f66a9df5629e660529788579
violet <101194343+joococo@users.noreply.github.com>
2023-09-29 21:02:28 +0900

    init

--- app/src/main/java/kr/ac/duksung/pongle/SelectSeat.java
+++ app/src/main/java/kr/ac/duksung/pongle/SelectSeat.java
@@ -1,39 +1,32 @@
 package kr.ac.duksung.pongle;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-
 public class SelectSeat extends Activity {
     Button choice;
-    ImageView exitButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_seat);
-        choice = findViewById(R.id.select_seat);
-        exitButton = findViewById(R.id.exitButton);
+        Intent getintent = getIntent();
+        Bundle bundle = getintent.getExtras();
+        String stdNum = bundle.getString("stdNum");
+        String stdID = bundle.getString("stdNum");
+        String seatID = bundle.getString("seatNum");
 
+
+        choice = findViewById(R.id.select_seat);
         choice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), ChooseMenu.class);
+                intent.putExtra("seatNum", seatID);
+                intent.putExtra("stdNum", stdID);
                 startActivity(intent);
-
-                //자리 정보 넣어주기
-                //orderData.setSeatID();
-            }
-        });
-
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainPage.class);
-                startActivity(intent);
+                intent.putExtra("stdNum", stdNum);
+                // intent.putExtra("seatID", seatNum);
             }
         });
     }
