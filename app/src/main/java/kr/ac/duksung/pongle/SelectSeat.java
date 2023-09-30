8dceee93593df72083df2a40c2292d35fe19d2d6
dusskdus <skdusb0007@naver.com>
2023-09-30 12:13:38 +0900

    button_setting_5

--- app/src/main/java/kr/ac/duksung/pongle/SelectSeat.java
+++ app/src/main/java/kr/ac/duksung/pongle/SelectSeat.java
@@ -20,12 +20,48 @@ public class SelectSeat extends AppCompatActivity {
 
         final ImageView seatButton1 = findViewById(R.id.seat_button_1);
         final ImageView choiceButton1 = findViewById(R.id.choice_button_1);
+        final ImageView seatButton2 = findViewById(R.id.seat_button_2);
+        final ImageView choiceButton2 = findViewById(R.id.choice_button_2);
+        final ImageView seatButton3 = findViewById(R.id.seat_button_3);
+        final ImageView choiceButton3 = findViewById(R.id.choice_button_3);
+        final ImageView seatButton4 = findViewById(R.id.seat_button_4);
+        final ImageView choiceButton4 = findViewById(R.id.choice_button_4);
+        final ImageView seatButton5 = findViewById(R.id.seat_button_5);
+        final ImageView choiceButton5 = findViewById(R.id.choice_button_5);
 
         seatButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // seat_button_1이 클릭되었을 때 choice_button_1를 화면에 보이도록 설정
                 choiceButton1.setVisibility(View.VISIBLE);
+            }
+        });
+        seatButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // seat_button_2가 클릭되었을 때 choice_button_2를 화면에 보이도록 설정
+                choiceButton2.setVisibility(View.VISIBLE);
+            }
+        });
+        seatButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // seat_button_3이 클릭되었을 때 choice_button_3를 화면에 보이도록 설정
+                choiceButton3.setVisibility(View.VISIBLE);
+            }
+        });
+        seatButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // seat_button_4가 클릭되었을 때 choice_button_4를 화면에 보이도록 설정
+                choiceButton4.setVisibility(View.VISIBLE);
+            }
+        });
+        seatButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // seat_button_5가 클릭되었을 때 choice_button_5를 화면에 보이도록 설정
+                choiceButton5.setVisibility(View.VISIBLE);
             }
         });
 
