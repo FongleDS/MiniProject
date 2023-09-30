960c7c1cc8817a3aa217fada50a999f4f11b4d70
dusskdus <skdusb0007@naver.com>
2023-09-30 09:17:59 +0900

    button_setting

--- app/src/main/java/kr/ac/duksung/pongle/SelectSeat.java
+++ app/src/main/java/kr/ac/duksung/pongle/SelectSeat.java
@@ -2,9 +2,11 @@ package kr.ac.duksung.pongle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-public class SelectSeat extends Activity {
+import android.widget.ImageView;
+public class SelectSeat extends AppCompatActivity {
     Button choice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,17 @@ public class SelectSeat extends Activity {
         String stdNum = bundle.getString("stdNum");
         String stdID = bundle.getString("stdNum");
         String seatID = bundle.getString("seatNum");
+
+        final ImageView seatButton1 = findViewById(R.id.seat_button_1);
+        final ImageView choiceButton2 = findViewById(R.id.choice_button_1);
+
+        seatButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // seat_button_1이 클릭되었을 때 choice_button_2를 화면에 보이도록 설정
+                choiceButton2.setVisibility(View.VISIBLE);
+            }
+        });
 
 
         choice = findViewById(R.id.select_seat);
