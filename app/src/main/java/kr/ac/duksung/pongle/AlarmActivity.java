fa70d79d7b7a17ba35a8b0077eb0a565b72653b0
dusskdus <skdusb0007@naver.com>
2023-10-03 13:30:44 +0900

    alarm_popup_make

--- app/src/main/java/kr/ac/duksung/pongle/AlarmActivity.java
+++ app/src/main/java/kr/ac/duksung/pongle/AlarmActivity.java
@@ -1,13 +1,29 @@
 package kr.ac.duksung.pongle;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AlarmActivity extends AppCompatActivity {
 
+    Button btn_alarm;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        btn_alarm = findViewById(R.id.button_alarm);
+
+        btn_alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
